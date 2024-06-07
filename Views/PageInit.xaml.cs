dbd99926edd13874396e9f956af9d05b134f3b64
Dell <ricardo.lagoshn@gmail.com>
2024-06-06 20:05:42 -0600

    Cambios al 06/06/2024

--- Views/PageInit.xaml.cs
+++ Views/PageInit.xaml.cs
@@ -2,10 +2,53 @@ namespace PM0220242P.Views;
 
 public partial class PageInit : ContentPage
 {
+    FileResult photo;
+
 	public PageInit()
 	{
 		InitializeComponent();
 	}
+
+
+    public String GetImage64()
+    {
+        String Base64 = String.Empty;
+
+        if (photo != null)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                Stream stream = photo.OpenReadAsync().Result;
+                stream.CopyTo(ms);
+                byte[] data = ms.ToArray();
+
+                Base64 = Convert.ToBase64String(data);
+                return Base64;
+            }
+        }
+
+        return Base64;
+    }
+
+
+    public byte[] GetImageArray()
+    {
+        byte[] data = new Byte[] { };
+
+        if (photo != null)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                Stream stream = photo.OpenReadAsync().Result;
+                stream.CopyTo(ms);
+                data = ms.ToArray();
+
+                return data;
+            }
+        }
+
+        return data;
+    }
 
     private async void btnaceptar_Clicked(object sender, EventArgs e)
     {
@@ -14,7 +57,8 @@ public partial class PageInit : ContentPage
             Nombres = Nombres.Text,
             Apellidos = Apellidos.Text,
             FechaNac = FechaNac.Date,
-            Telefono = Telefono.Text
+            Telefono = Telefono.Text,
+            Foto = GetImage64()
         };
 
         if (await App.DataBase.StorePerson(person) > 0)
@@ -23,8 +67,28 @@ public partial class PageInit : ContentPage
         }
     }
 
-    private void btnfoto_Clicked(object sender, EventArgs e)
+    private async void btnfoto_Clicked(object sender, EventArgs e)
     {
+        try
+        {
+            photo = await MediaPicker.Default.CapturePhotoAsync();
 
+            if (photo != null)
+            {
+                string Localizacion = Path.Combine(FileSystem.CacheDirectory, photo.FileName);
+
+                using Stream sourceStream = await photo.OpenReadAsync();
+                using FileStream imagenlocal = File.OpenWrite(Localizacion);
+
+                FotoImage.Source = ImageSource.FromStream(() => photo.OpenReadAsync().Result);
+
+                await sourceStream.CopyToAsync(imagenlocal);
+
+            }
+        }
+        catch (Exception ex) 
+        {
+            ex.ToString();
+        }
     }
 }
